69b82d8992430b5e2f986b73c2b04a5491ff6ba9
Zach Leslie <xaque208@gmail.com>
2020-04-24 23:43:51 -0700

    add script to bump port version

---
+++ bump_vesion.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+set -e
+
+VERSION=$1
+PORT=$2
+
+if [ -z "$VERSION" ]; then
+    echo "version required"
+fi
+
+if [ -z "$PORT" ]; then
+    echo "port required"
+fi
+
+TAG_VER=$(echo $VERSION | sed -e '/^v/s/^v\(.*\)$/\1/g')
+
+pushd $PORT
+    sed -i  -e "s/PORTVERSION=.*/PORTVERSION=	$TAG_VER/g" Makefile
+    rm -f distinfo
+    make makesum
+    git add distinfo
+    git add Makefile
+    git ci -m "chore: update ${PORT} for ${VERSION}"
+    git push
+popd
+
+sudo -i bash -c portshaker -v
+
+tree=personal
+jail=larch12
+list=/home/zach/Code/personal-ports/personal.list
+
+sudo poudriere bulk -f $list -p $tree -j $jail
