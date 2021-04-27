460e6823af0343ffc003e8328e4c02d874521eab
Zach Leslie <xaque208@gmail.com>
2021-04-26 19:42:25 -0700

    Update jails

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 sudo -i portshaker -v
 
 tree=personal
-jail=larch12
+jails=(larch13 larch12)
 list=/home/zach/Code/personal-ports/personal.list
+
+for jail in "${jails[@]}"; do
+    sudo poudriere bulk -f /usr/local/etc/poudriere.d/$build.list -p $tree -j $build
+done
 
 sudo poudriere bulk -f $list -p $tree -j $jail
