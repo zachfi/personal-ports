77b4e9e2de84ff370c73afe8a6706ad4db2d252c
Zach Leslie <xaque208@gmail.com>
2021-05-02 22:17:25 -0700

    Bump jail verison

--- bump_vesion.sh
+++ bump_vesion.sh
@@ -28,7 +28,7 @@ popd
 sudo -i bash -c portshaker -v
 
 tree=personal
-jail=larch12
+jail=larch13
 list=/home/zach/Code/personal-ports/personal.list
 
 sudo poudriere bulk -f $list -p $tree -j $jail
