f9252101065fed339977eaba81b22075668290e2
Zach Leslie <xaque208@gmail.com>
2021-05-10 10:23:13 -0700

    Undo setting the loki user

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ jails=(larch13 larch12)
 list=/home/zach/Code/personal-ports/personal.list
 
 for jail in "${jails[@]}"; do
-    sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail
+    # sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail
+    sudo poudriere bulk -f $list -p $tree -j $jail
 done
-
-sudo poudriere bulk -f $list -p $tree -j $jail
