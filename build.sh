469ebe35fe463f37a996059f95edb19df2aa074b
Zach Leslie <xaque208@gmail.com>
2022-05-27 18:35:02 +0000

    Reduce parallel builds

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ list=/home/zach/Code/personal-ports/personal.list
 
 for jail in "${jails[@]}"; do
     # sudo poudriere bulk -f /usr/local/etc/poudriere.d/$jail.list -p $tree -j $jail
-    sudo poudriere bulk -f $list -p $tree -j $jail
+    sudo poudriere bulk -f $list -p $tree -j $jail -J 2
 done
