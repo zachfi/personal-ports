84ad3786461326f572d16d09230d69eb332cd38f
Zach Leslie <xaque208@gmail.com>
2025-08-13 13:29:28 +0000

    Updates

--- bump_vesion.sh
+++ bump_vesion.sh
@@ -32,7 +32,7 @@ popd
 sudo -i bash -c portshaker -v
 
 tree=personal
-jail=pkg14-2
+jail=pkg14-3
 list=/home/zach/Code/personal-ports/personal.list
 
 #sudo poudriere bulk -f $list -p $tree -j $jail
